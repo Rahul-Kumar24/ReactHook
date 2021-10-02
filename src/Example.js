d522178ef415a80c1cecdb1e878a560f7a96913d
Rahul-Kumar24 <ravikroy582002@gmail.com>
2021-10-03 05:29:34 +0530

    update

--- src/Example.js
+++ src/Example.js
@@ -1,12 +1,20 @@
 import React, {useEffect} from 'react'
 
-const Example = () => {
+const Example = (props) => {
+          // const item=props.item.name;
+          // const count = props.item.count;
+          // console.log(count)
+          // console.log("Befor useEffect  ", item)
           useEffect(() =>{
-                    console.log("randre");
-          })
+                    // console.log("Inside useEffect  ", item);
+                    return ()=>{
+                              // console.log("inside function return ",item)
+                    }
+          },[])
+          console.log("After useEffect ",item)
           return (
                     <div>
-                              
+                              {console.log("withen div function ",item)}
                     </div>
           )
 }
--- src/Wrapper.js
+++ src/Wrapper.js
@@ -6,12 +6,13 @@ const Wrapper = () => {
           const onClickhandle = () => {
                     setCount(count+1);
           }
+          const name = "Rahul"
           return (
                     <div className="wrapper">
                               <h1>{count}</h1>
                               <br/>
-                              < button onClick={onClickhandle}>click</button>
-                              <Example/>
+                              <button type="button" class="btn btn-primary" onClick={onClickhandle}>Click</button>
+                              {count>1&&<Example count = {count}/>}
                     </div>
           )
 }
