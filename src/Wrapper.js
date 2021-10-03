27b42b26892e75d44c726023f22e3703bbdcf63a
Rahul-Kumar24 <ravikroy582002@gmail.com>
2021-10-03 15:08:32 +0530

    update4

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -6,13 +6,17 @@ const Wrapper = () => {
           const onClickhandle = () => {
                     setCount(count+1);
           }
+          // useEffect(()=>{
+          //           console.log("hello");
+          // },[])
           const name = "Rahul"
           return (
                     <div className="wrapper">
                               <h1>{count}</h1>
                               <br/>
                               <button type="button" class="btn btn-primary" onClick={onClickhandle}>Click</button>
-                              {count>1&&<Example count = {count}/>}
+                              {/* {count<5&&<Example count = {count}/>} */}
+                              {/* {count < 5 && <Example count={-1} />} */}
                     </div>
           )
 }
